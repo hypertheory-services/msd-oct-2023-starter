dd174105bcb667a3855c9df7d612731734ce315c
Jeff Gonzalez <Jeff@hypertheory.com>
2023-10-15 10:55:14 -0400

    Initial

---
+++ PortalDomain/PortalSolution/Portal.Api/SoftwareApi/Handlers/SoftwareHandler.cs
@@ -0,0 +1,39 @@
+﻿using Marten;
+
+using Portal.Api.SoftwareApi.Comands;
+using Portal.Api.SoftwareApi.Entities;
+using Portal.Api.SoftwareApi.Events;
+
+using Wolverine.Attributes;
+
+namespace Portal.Api.SoftwareApi.Handlers;
+
+public static class SoftwareHandler
+{
+
+    public static async Task<SoftwareEntity> Handle(CreateSoftware command, IDocumentSession session, CancellationToken token)
+    {
+        var software = new SoftwareEntity(Guid.NewGuid(), command.Title, command.SourceId);
+        session.Store(software);
+        await session.SaveChangesAsync();
+        return software;
+    }
+
+    public static async Task<IReadOnlyList<SoftwareEntity>> HandleAsync(GetSoftware _, IDocumentSession session)
+    {
+        var software = await GetActiveSoftware(session).ToListAsync();
+        return software;
+    }
+
+    public static async Task<SoftwareEntity?> HandleAsync(GetSoftwareById command, IDocumentSession session)
+    {
+        var software = await GetActiveSoftware(session).SingleOrDefaultAsync(s => s.Id == command.Id);
+        return software;
+    }
+
+    private static IQueryable<SoftwareEntity> GetActiveSoftware(IDocumentSession session)
+    {
+       return session.Query<SoftwareEntity>().Where(s => s.Retired == false);
+    }
+
+}
